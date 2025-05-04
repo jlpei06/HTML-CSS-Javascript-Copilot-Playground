644a7c609111e6f1ad7c16f40c646f0db63dbd27
jlpei06 <jlpei06@gmail.com>
2025-05-04 01:03:08 +0000

    Spiced Chia Pudding Round 1

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 console.log("Landing page loaded");
+<script src="script.js"></script>
+</body>
+</html>
+<section class="ingredients">
+  <h2>Ingredients</h2>
+ 
+      <ul>
+        <li>1/2 cup chia seeds</li>
+        <li>15 oz light coconut milk (1 can)</li>
+        <li>1/2 cup oat milk</li>
+        <li>3 tablespoons maple syrup</li>
+        <li>1/2 teaspoon vanilla extract</li>
+        <li>1/2 teaspoon cinnamon</li>
+        <li>1/4 teaspoon sea salt</li>
+        <li>1/4 teaspoon cardamom (optional)</li>
+
+       
+
+          </ul>
+        </section>
+      </ul>
+    </section>
+  </ul>
+</section>
+<section class="ingredients">
+<h2>Directions</h2>
+
+<div style="margin-left: 30px;">
+
+1. In a large bowl, mix all ingredients together for 5 minutes.  
+<br>
+2. Let sit for 5 minutes.  
+<br>
+3. Stir again to prevent clumping.  
+<br>
+4. Refrigerate for at least 4 hours or overnight.  
+<br>
+5. Top with fruit and enjoy!
+
+</div>
